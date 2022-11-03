ee1b31db4bbf7c1cd54fcfd199f8135689ed596e
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-03 18:27:01 +0100

    Add book and form components to the books component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
 
-const Form = () => (
-  <div>Form</div>
-);
+const Form = () => {
+  const handleSubmit = () => {
+    console.log('submitted');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add New Book..."
+        value="book"
+        name="book"
+      />
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add Author..."
+        value="author"
+        name="author"
+      />
+      <button type="submit" className="input-submit">
+        Add New Book
+      </button>
+    </form>
+  );
+};
 
 export default Form;
