f77c7a5f27aac0a7d11d45f0b2ae43be66f30fe5
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-07 22:04:14 +0100

    Add bookSlice

---
+++ src/features/configureStore.js
@@ -0,0 +1,10 @@
+import { configureStore } from '@reduxjs/toolkit';
+
+const store = configureStore({
+  reducer: {
+    book: bookReducer,
+    category: categoryReducer,
+  },
+});
+
+export default store;
