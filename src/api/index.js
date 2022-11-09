35894feb9de53c71a24604f69e78b8dce8eea80e
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-09 11:38:18 +0100

    Fix typo

--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const API = axios.create({ baseURL: 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/app/abc234/' });
 
-export const createPost = (bookData) => API.post('/books', bookData);
-export const getPosts = () => API.get('/books');
+export const createBook = (bookData) => API.post('/books', bookData);
+export const getBook = () => API.get('/books');
 export const deleteBook = (id) => API.delete(`/books/${id}`);
