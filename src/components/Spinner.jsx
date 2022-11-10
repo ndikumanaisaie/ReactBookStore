8b3e934efa8edac6002676b733fe289852732b92
ndikumanaisaie <ndikumanaisaie@gmail.com>
2022-11-10 23:44:37 +0100

    Remove inline styling for spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { Audio } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 
 const Spinner = () => (
   <div className="spinner">
-    <Audio
-      height="80"
-      width="80"
-      radius="9"
-      color="green"
-      ariaLabel="loading"
-      wrapperStyle
-      wrapperClass
-    />
+    <RotatingLines />
   </div>
 );
 
